300ede8a0498faab7bc48fa258d048086a79e750
Vivian Oh <116562546+voh919@users.noreply.github.com>
2025-04-30 20:46:56 -0400

    starter code and lexer

---
+++ Lexer.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class Lexer {
+    public ArrayList<String> tokenize(String input) {
+        Set<Character> symbols = Set.of('(', ')', '\\', 'λ', '.', '=', ';');
+        ArrayList<String> tokens = new ArrayList<>();
+        String token = "";
+
+        for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+
+            if (c == ';') break;
+
+            if (Character.isWhitespace(c)) {
+                if (!token.isEmpty()) {
+                    tokens.add(token);
+                    token = "";
+                }
+            } else if (symbols.contains(c)) {
+                if (!token.isEmpty()) {
+                    tokens.add(token);
+                    token = "";
+                }
+                tokens.add(String.valueOf(c));
+            } else {
+                token += c;
+            }
+        }
+
+        if (!token.isEmpty()) {
+            tokens.add(token);
+        }
+        
+        return tokens;
+    }
+}
+
+
